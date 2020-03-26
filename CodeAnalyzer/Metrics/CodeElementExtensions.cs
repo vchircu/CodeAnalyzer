fea78bc32a31ad87c71b3fd973d489b19fd6c58c
Victor Chircu <Victor.Chircu@gmail.com>
2020-03-26 16:28:11 +0200

    Update algorithm for HierarchyRelations calculation

--- CodeAnalyzer/Metrics/CodeElementExtensions.cs
+++ CodeAnalyzer/Metrics/CodeElementExtensions.cs
@@ -15,6 +15,18 @@
             return method.IsPropertyGetter || method.IsPropertySetter;
         }
 
+        public static bool IsFunctional(this ICodeElement codeElement)
+        {
+            if (!codeElement.IsMethod)
+            {
+                return false;
+            }
+
+            var method = codeElement.AsMethod;
+
+            return !method.IsProperty() && !method.IsConstructor;
+        }
+
         public static bool IsPropertyOrField(this ICodeElement codeElement)
         {
             return codeElement.IsProperty() || codeElement.IsField;
--- CodeAnalyzer/StructuralRelations/StructuralRelation.cs
+++ CodeAnalyzer/StructuralRelations/StructuralRelation.cs
@@ -1,5 +1,6 @@
 ﻿namespace CodeAnalyzer.StructuralRelations
 {
+    using System.Collections.Generic;
     using System.Linq;
 
     using CodeAnalyzer;
@@ -8,6 +9,7 @@
     using CSharpFunctionalExtensions;
 
     using NDepend.CodeModel;
+    using NDepend.Helpers;
 
     public class StructuralRelation
     {
@@ -32,10 +34,16 @@
                 return Maybe<StructuralRelation>.None;
             }
 
-            int hierarchySpecificRelations = ComputeHierarchyRelations(source, target);
-            var externalCalls = hierarchySpecificRelations > 0 ? 0 : target.Methods.Count(m => !m.IsPropertyOrField() && source.IsUsingMethod(m));
-            var externalData = hierarchySpecificRelations > 0 ? 0 : target.Members.Count(m => m.IsPropertyOrField() && source.IsUsing(m));
+            var isInnerClass = source.ChildTypes.Contains(target);
 
+            HashSet<IMethod> hierarchySpecificRelatedMethods = HierarchySpecificRelatedMethods(source, target);
+            var externalCalls = isInnerClass ? 0 : target.Methods.Count(m => m.IsFunctional() && source.IsUsingMethod(m) && !hierarchySpecificRelatedMethods.Contains(m));
+
+            HashSet<IMember> hierarchyDataAccess = HierarchyDataAccess(source, target);
+            var externalData = isInnerClass ? 0 : target.Members.Count(m => m.IsPropertyOrField() && source.IsUsing(m) && !hierarchyDataAccess.Contains(m));
+
+            var hierarchySpecificRelations = HierarchySpecificRelations(source, target, hierarchySpecificRelatedMethods, hierarchyDataAccess);
+           
             if (hierarchySpecificRelations == 0 && externalCalls == 0 && externalData == 0)
             {
                 return Maybe<StructuralRelation>.None;
@@ -51,9 +59,38 @@
             });
         }
 
-        private static int ComputeHierarchyRelations(IType source, IType target)
+        private static int HierarchySpecificRelations(
+            IType source,
+            IType target,
+            HashSet<IMethod> hierarchySpecificRelatedMethods,
+            HashSet<IMember> hierarchyDataAccess)
         {
-            return source.HierarchyIncludingImplementedInterfaces().Contains(target) ? 0 : 1;
+            var isBaseClass = source.BaseClasses.Contains(target);
+            var isImplementedInterface = source.InterfacesImplemented.Contains(target);
+            var hierarchySpecificRelations = (isBaseClass ? 1 : 0) + (isImplementedInterface ? 1 : 0)
+                                                                   + hierarchySpecificRelatedMethods.Count
+                                                                   + hierarchyDataAccess.Count;
+            return hierarchySpecificRelations;
+        }
+
+        private static HashSet<IMethod> HierarchySpecificRelatedMethods(IType source, IType target)
+        {
+            return AllBaseOverriddenMethods(source, target).Concat(HierarchyMethodCalls(source, target)).ToHashSetEx();
+        }
+
+        private static HashSet<IMember> HierarchyDataAccess(IType source, IType target)
+        {
+            return source.Methods.SelectMany(m => m.MembersUsed).Where(m => m.ParentType == target && m.IsPropertyOrField() && m.IsProtected).ToHashSetEx();
+        }
+
+        private static HashSet<IMethod> HierarchyMethodCalls(IType source, IType target)
+        {
+            return source.Methods.SelectMany(m => m.MethodsCalled).Where(m => m.ParentType == target && m.IsProtected && m.IsFunctional()).ToHashSetEx();
+        }
+
+        private static HashSet<IMethod> AllBaseOverriddenMethods(IType source, IType target)
+        {
+            return source.Methods.SelectMany(m => m.OverriddensBase).Where(m => m.ParentType == target).ToHashSetEx();
         }
     }
 }
