597d899388e556656aff46fab98e84808aada278
Victor Chircu <Victor.Chircu@gmail.com>
2020-03-30 12:49:14 +0300

    Added Severity calculation for Feature Envy

--- CodeAnalyzer/DetectionStrategies/FeatureEnvy.cs
+++ CodeAnalyzer/DetectionStrategies/FeatureEnvy.cs
@@ -1,6 +1,7 @@
 ﻿namespace CodeAnalyzer.DetectionStrategies
 {
     using CodeAnalyzer.Metrics;
+    using CodeAnalyzer.Thresholds;
 
     using CSharpFunctionalExtensions;
 
@@ -8,22 +9,30 @@
 
     public class FeatureEnvy : IDetectMethodDesignSmell
     {
+        private const int Few = 4;
+
         public Maybe<DesignSmell> Detect(IMethod m)
         {
             var atfd = AccessToForeignDataForMethod.Value(m);
             var laa = LocalityOfAttributeAccess.Value(m);
             var fdp = ForeignDataProviders.Value(m);
 
-            if (atfd > 5 && laa < 0.33 && fdp > 0 && fdp <= 3)
+            if (atfd > Few && laa < CommonFractionThreshold.OneThird && fdp > 0 && fdp <= 3)
             {
                 return Maybe<DesignSmell>.From(
                     new DesignSmell
                         {
-                            Name = "Feature Envy", Severity = atfd, SourceFile = m.ParentType.SourceFile(), Source = m
+                            Name = "Feature Envy", Severity = SeverityFor(atfd), SourceFile = m.ParentType.SourceFile(), Source = m
                         });
             }
 
             return Maybe<DesignSmell>.None;
+        }
+
+        private static double SeverityFor(int atfd)
+        {
+            var linearNormalization = LinearNormalization.WithMeasurementAndDesiredRange(Few, 20, 1, 10);
+            return linearNormalization.ValueFor(atfd);
         }
     }
 }
