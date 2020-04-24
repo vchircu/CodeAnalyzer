430ad67e0b89f3870d57e125243e04dc2ecbb937
Victor Chircu <Victor.Chircu@gmail.com>
2020-04-24 11:00:11 +0300

    Group method smells by source file

--- CodeAnalyzer/DetectionStrategies/DesignSmellsDetection.cs
+++ CodeAnalyzer/DetectionStrategies/DesignSmellsDetection.cs
@@ -37,39 +37,42 @@
 
         public IEnumerable<DesignSmell> Detect(ICodeBase codeBase)
         {
-            _logger.Information("\tDetecting Design Smells in types");
+            IEnumerable<DesignSmell> typeDesignSmells = DetectTypeDesignSmells(codeBase);
 
+            IEnumerable<DesignSmell> methodDesignSmells = DetectMethodDesignSmells(codeBase);
+
+            return typeDesignSmells.Concat(methodDesignSmells);
+        }
+
+        private static IEnumerable<DesignSmell> GroupByDesignSmellAndSourceFile(List<DesignSmell> methodDesignSmells)
+        {
+            return methodDesignSmells.GroupBy(s => s.SourceFile + s.Name).Select(
+                g => new DesignSmell
+                         {
+                             SourceFile = g.First().SourceFile,
+                             Name = g.First().Name,
+                             Severity = g.Sum(d => d.Severity),
+                             Source = g.First().Source
+                         });
+        }
+
+        private static bool IsDefaultConstructorGeneratedByCompiler(IMethod m)
+        {
+            return m.IsConstructor && m.NbParameters == 0 && (m.IsPublic || m.IsProtected && m.ParentType.IsAbstract)
+                   && !m.SourceFileDeclAvailable;
+        }
+
+        private static bool IsGenerated(ICodeBase codeBase, IType type)
+        {
+            var generatedCodeAttributeType = codeBase.ThirdParty.Types
+                .WithFullName("System.CodeDom.Compiler.GeneratedCodeAttribute").SingleOrDefault();
+            return generatedCodeAttributeType != null && type.HasAttribute(generatedCodeAttributeType);
+        }
+
+        private IEnumerable<DesignSmell> DetectMethodDesignSmells(ICodeBase codeBase)
+        {
+            var methodDesignSmells = new List<DesignSmell>();
             var stopWatch = Stopwatch.StartNew();
-
-            foreach (var t in codeBase.Application.Types)
-            {
-                _logger.Debug("\t\t" + t.FullName);
-
-                if (string.IsNullOrWhiteSpace(t.SourceFile()))
-                {
-                    continue;
-                }
-
-                if (IsGenerated(codeBase, t))
-                {
-                    continue;
-                }
-
-                foreach (var typeDesignSmellDetectionStrategy in _typeDesignSmellDetectionStrategies)
-                {
-                    Maybe<DesignSmell> designSmell = typeDesignSmellDetectionStrategy.Detect(t);
-                    if (designSmell.HasValue)
-                    {
-                        yield return designSmell.Value;
-                    }
-                }
-            }
-
-            stopWatch.Stop();
-
-            _logger.Information("\tDetected Design Smells in types in {Elapsed:000} ms", stopWatch.ElapsedMilliseconds);
-
-            stopWatch = Stopwatch.StartNew();
             _logger.Information("\tDetecting Design Smells in methods");
             foreach (var m in codeBase.Application.Methods)
             {
@@ -95,7 +98,7 @@
                     Maybe<DesignSmell> designSmell = methodDesignSmellDetectionStrategy.Detect(m);
                     if (designSmell.HasValue)
                     {
-                        yield return designSmell.Value;
+                        methodDesignSmells.Add(designSmell.Value);
                     }
                 }
             }
@@ -104,19 +107,46 @@
             _logger.Information(
                 "\tDetected Design Smells in methods in {Elapsed:000} ms",
                 stopWatch.ElapsedMilliseconds);
+
+            IEnumerable<DesignSmell> groupedDesignSmells = GroupByDesignSmellAndSourceFile(methodDesignSmells);
+            return groupedDesignSmells;
         }
 
-        private static bool IsDefaultConstructorGeneratedByCompiler(IMethod m)
+        private IEnumerable<DesignSmell> DetectTypeDesignSmells(ICodeBase codeBase)
         {
-            return m.IsConstructor && m.NbParameters == 0 && (m.IsPublic || m.IsProtected && m.ParentType.IsAbstract)
-                   && !m.SourceFileDeclAvailable;
-        }
+            _logger.Information("\tDetecting Design Smells in types");
 
-        private static bool IsGenerated(ICodeBase codeBase, IType type)
-        {
-            var generatedCodeAttributeType = codeBase.ThirdParty.Types
-                .WithFullName("System.CodeDom.Compiler.GeneratedCodeAttribute").SingleOrDefault();
-            return generatedCodeAttributeType != null && type.HasAttribute(generatedCodeAttributeType);
+            var stopWatch = Stopwatch.StartNew();
+
+            var typeDesignSmells = new List<DesignSmell>();
+            foreach (var t in codeBase.Application.Types)
+            {
+                _logger.Debug("\t\t" + t.FullName);
+
+                if (string.IsNullOrWhiteSpace(t.SourceFile()))
+                {
+                    continue;
+                }
+
+                if (IsGenerated(codeBase, t))
+                {
+                    continue;
+                }
+
+                foreach (var typeDesignSmellDetectionStrategy in _typeDesignSmellDetectionStrategies)
+                {
+                    Maybe<DesignSmell> designSmell = typeDesignSmellDetectionStrategy.Detect(t);
+                    if (designSmell.HasValue)
+                    {
+                        typeDesignSmells.Add(designSmell.Value);
+                    }
+                }
+            }
+
+            stopWatch.Stop();
+
+            _logger.Information("\tDetected Design Smells in types in {Elapsed:000} ms", stopWatch.ElapsedMilliseconds);
+            return typeDesignSmells;
         }
     }
 }
